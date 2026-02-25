291d6919928fcb32aa337f465caf25eee1d29081
Hizrawan <Hizrawandwioka@gmail.com>
2026-02-25 22:55:21 +0800

    using struct to make the code more readable

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -7,30 +7,29 @@
 
 import SwiftUI
 
+struct DataItemModel{
+    let text:String
+    let color:Color
+}
+
 struct ContentView: View {
+    let messages:[DataItemModel] = [
+        .init(text: "Hi everyone,", color: .green),
+        .init(text: "my name is", color: .gray),
+        .init(text: "Hizrawan Dwi Oka", color: .yellow),
+        .init(text: "I am from Bandung, Indonesia", color: .red),
+        .init(text: "Nice to meet you all !!", color: .purple)
+    ]
     var body: some View {
         
         VStack(alignment: .leading) {
-                TextView(
-                    text: "Hi everyone,",
-                    color: .green
-                )
-                TextView(
-                    text: "my name is",
-                    color: .gray
-                )
-                TextView(
-                    text: "Hizrawan Dwi Oka",
-                    color: .yellow
-                )
-                TextView(
-                    text: "I am from Bandung, Indonesia",
-                    color: .red
-                )
-                TextView(
-                    text: "Nice to meet you all !!",
-                    color: .purple
-                )
+               
+            TextView(text: messages[0].text, color: messages[0].color)
+            TextView(text: messages[1].text, color: messages[1].color)
+            TextView(text: messages[2].text, color: messages[2].color)
+            TextView(text: messages[3].text, color: messages[3].color)
+            TextView(text: messages[4].text, color: messages[4].color)
+            
         }
     }
 }
