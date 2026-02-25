e6a67c6d79aa433ce46f07394a0000bef7676797
Hizrawan <Hizrawandwioka@gmail.com>
2026-02-25 21:35:33 +0800

    Initial Commit

---
+++ Greetings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greetings
+//
+//  Created by macminim1 on 2026/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by macminim1 on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
